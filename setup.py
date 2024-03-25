532a818d40d1fac9defee40166335257dc21d052
Maanav15 <142615135+Maanav15@users.noreply.github.com>
2024-03-25 20:55:16 +0530

    Setup.py places

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import find_packages,setup
+from typing import  List
+
+H = '-e .'
+
+def get_requirements(file_path:str)->List[str]:
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements = [req.replace("/n","")for req in requirements]
+        if H in requirements:
+            requirements.remove(H)
+    return requirements
+
+setup(
+name='ML_project',
+version= '0.1',
+author='Maanav Agarwal',
+author_email='agarwalmanav278@gmail.com',
+packages=find_packages(),
+install_requires=get_requirements('requirements.txt')
+)
